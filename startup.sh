5d18450104995fbb23f78dcf572400fd676173e6
Jsh Agarwal <jshagarwal15@gmail.com>
2025-03-02 15:49:32 +0530

    hopefully final fix

--- startup.sh
+++ startup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 cd /home/site/wwwroot
-gunicorn --bind=0.0.0.0 --timeout 600 api.main:app --workers 4 --access-logfile '-' --error-logfile '-'
+
+echo "Current directory: $(pwd)"
+echo "Python version: $(python --version)"
+echo "Pip version: $(pip --version)"
+echo "Installing dependencies..."
+python -m pip install -r requirements.txt
+
+echo "Starting Gunicorn..."
+gunicorn --bind=0.0.0.0:8000 --timeout 600 api.main:app --workers 4 --access-logfile '-' --error-logfile '-' --log-level debug
