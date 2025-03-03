85d96de59d5290f74ead715ee09ddaa8a1f833bc
Jsh Agarwal <jshagarwal15@gmail.com>
2025-03-03 08:39:16 +0530

    removed deployment

--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-bind = "0.0.0.0:8000"
-workers = 4
-timeout = 600
-loglevel = "debug"
-capture_output = True
-enable_stdio_inheritance = True
-preload_app = True
-worker_class = "uvicorn.workers.UvicornWorker"
